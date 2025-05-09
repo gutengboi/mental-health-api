8793cf5f5a3edc23a5c42bcc33964c495599a0b3
gutengboi <animashaunolayinka195@gmail.com>
2025-05-09 07:07:10 +0100

    dummy clients and therapist added

--- src/models/user.ts
+++ src/models/user.ts
@@ -11,6 +11,21 @@ export interface User {
 }
 
 // Added only for testing purposes
+// export const USERS: User[] = [
+//   {
+//     email: 'client@galaxies.dev',
+//     id: '7z6ydcm',
+//     role: UserRole.Client,
+//     hashed_password: '$2b$10$.MftzcPPsR5TUTYRyWGyQu9H9Fd3Q6olBlccI1qIAY3qXH7OQ.bQO', // dummy "123456"
+//   },
+//   {
+//     email: 'simon@galaxies.dev',
+//     id: '41m3lxk',
+//     role: UserRole.Therapist,
+//     hashed_password: '$2b$10$.MftzcPPsR5TUTYRyWGyQu9H9Fd3Q6olBlccI1qIAY3qXH7OQ.bQO', // dummy "123456"
+//   },
+// ];
+
 export const USERS: User[] = [
   {
     email: 'client@galaxies.dev',
@@ -21,6 +36,42 @@ export const USERS: User[] = [
   {
     email: 'simon@galaxies.dev',
     id: '41m3lxk',
+    role: UserRole.Therapist,
+    hashed_password: '$2b$10$.MftzcPPsR5TUTYRyWGyQu9H9Fd3Q6olBlccI1qIAY3qXH7OQ.bQO', // dummy "123456"
+  },
+  {
+    email: 'anikala@galaxies.dev',
+    id: 'a9k2bnd',
+    role: UserRole.Client,
+    hashed_password: '$2b$10$.MftzcPPsR5TUTYRyWGyQu9H9Fd3Q6olBlccI1qIAY3qXH7OQ.bQO', // dummy "123456"
+  },
+  {
+    email: 'benson@galaxies.dev',
+    id: 'c2x5kqp',
+    role: UserRole.Client,
+    hashed_password: '$2b$10$.MftzcPPsR5TUTYRyWGyQu9H9Fd3Q6olBlccI1qIAY3qXH7OQ.bQO', // dummy "123456"
+  },
+  {
+    email: 'kunle@muj.dev',
+    id: 'e4z9mfj',
+    role: UserRole.Client,
+    hashed_password: '$2b$10$.MftzcPPsR5TUTYRyWGyQu9H9Fd3Q6olBlccI1qIAY3qXH7OQ.bQO', // dummy "123456"
+  },
+  {
+    email: 'ajala@benson.dev',
+    id: 'g7y1bdv',
+    role: UserRole.Client,
+    hashed_password: '$2b$10$.MftzcPPsR5TUTYRyWGyQu9H9Fd3Q6olBlccI1qIAY3qXH7OQ.bQO', // dummy "123456"
+  },
+  {
+    email: 'DrRay@galaxies.dev',
+    id: 'm3z8nxl',
+    role: UserRole.Therapist,
+    hashed_password: '$2b$10$.MftzcPPsR5TUTYRyWGyQu9H9Fd3Q6olBlccI1qIAY3qXH7OQ.bQO', // dummy "123456"
+  },
+  {
+    email: 'Drkol@galaxies.dev',
+    id: 'u5q6plv',
     role: UserRole.Therapist,
     hashed_password: '$2b$10$.MftzcPPsR5TUTYRyWGyQu9H9Fd3Q6olBlccI1qIAY3qXH7OQ.bQO', // dummy "123456"
   },
